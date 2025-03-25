b5cc04a4cb47bc6c910c62e1a1aad243d5cd8d52
Jacob <jayhoukes@gmail.com>
2025-03-25 15:06:27 +0100

    add encoding, split open logic

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,7 +3,7 @@ import json
 
 def load_data(file_path):
     """ Loads a JSON file """
-    with (open(file_path, "r") as handle):
+    with (open(file_path, "r", encoding='utf-8') as handle):
         return json.load(handle)
 
 
@@ -27,20 +27,26 @@ def get_animal_info(animals_list):
     return name_diet_loc_type
 
 
-def main():
-    """This function reads animal data from 'animals_data.json', inserts it into 'animals_template.html',
-    and writes the result to 'new_animals_template.html'."""
-
-    animals_data = load_data('animals_data.json')
-    formatted_data = get_animal_info(animals_data)
-
-    with open("animals_template.html", "r") as file:
+def read_html(file_path, formatted_data):
+    with open(file_path, "r", encoding='utf-8') as file:
         animals_template_content = file.read()
         actual_animal_data = animals_template_content.replace("__REPLACE_ANIMALS_INFO__", formatted_data)
-        print(actual_animal_data)
+        return actual_animal_data
 
-    with open("new_animals_template.html", "w") as file:
-        file.write(actual_animal_data)
+
+def write_html(file_path, new_content):
+    with open(file_path, "w", encoding='utf-8') as file:
+        file.write(new_content)
+
+
+def main():
+    """This function reads animal data from 'animals_data.json',
+    inserts it into 'animals_template.html',
+    and writes the result to 'new_animals_template.html'."""
+    animals_data = load_data('animals_data.json')
+    formatted_data = get_animal_info(animals_data)
+    filled_animal_template = read_html("animals_template.html", formatted_data)
+    write_html("new_animals_template.html", filled_animal_template)
 
 
 if __name__ == "__main__":
